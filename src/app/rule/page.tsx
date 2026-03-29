6a13854718599e8fb8cf1ba2cda405df102dace1
LeoHsxg <116825362+LeoHsxg@users.noreply.github.com>
2026-03-30 00:39:05 +0800

    fix: rename src/pages to src/views, update app route imports

--- src/app/rule/page.tsx
+++ src/app/rule/page.tsx
@@ -1,4 +1,4 @@
 'use client'
 import dynamic from 'next/dynamic'
-const Rule = dynamic(() => import('../../pages/Rule'), { ssr: false })
+const Rule = dynamic(() => import('../../views/Rule'), { ssr: false })
 export default Rule
