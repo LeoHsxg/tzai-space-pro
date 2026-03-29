280ad2c3473af15ecff2d3d4f10fda348cd3a27d
LeoHsxg <116825362+LeoHsxg@users.noreply.github.com>
2026-03-30 00:13:09 +0800

    chore: add 'use client' to all page components

--- src/pages/ApplyForm.tsx
+++ src/pages/ApplyForm.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useState, useEffect } from "react";
 import { Button, Box, MenuItem, FormControl, Select } from "@mui/material";
 import InputLabel from "@mui/material/InputLabel";
