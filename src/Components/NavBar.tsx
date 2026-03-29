7f4f8b8138d12aae38849dc2fe3b06186b8ff46e
LeoHsxg <116825362+LeoHsxg@users.noreply.github.com>
2026-03-30 00:13:09 +0800

    feat: replace SVG module import with public path in NavBar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import SignBt from "./SignBt";
-import Logo from "../img/LOGO_9x3.svg";
-import NavLinks from "./NavLinks";
+'use client'
+
+import React from 'react'
+import SignBt from './SignBt'
+import NavLinks from './NavLinks'
+
 const NavBar = () => {
   return (
-    // md:my-5  md:mx-4
     <div className="flex w-full h-20 px-8 py-4 md:h-16 justify-between items-center z-50 md:border-solid border-b-2 border-gray-200">
-      <img className="w-16 h-6" src={Logo} />
+      <img className="w-16 h-6" src="/img/LOGO_9x3.svg" alt="Logo" />
       <div className="flex items-center gap-4">
         <div className="hidden md:block">
           <NavLinks />
@@ -14,7 +15,7 @@ const NavBar = () => {
         <SignBt />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
